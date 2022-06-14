86ed8093210cd2ca49a556b07b76b5301875e87b
Yan Oliveira <yoliveira@indracompany.com>
2022-06-13 23:35:47 -0300

    ...

---
+++ CleanArch.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
