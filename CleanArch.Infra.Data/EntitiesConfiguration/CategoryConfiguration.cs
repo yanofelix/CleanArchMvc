3e431ef0eec46cc524e75a44aa028ba9e1f2caec
yanofelix <devyanfelix@gmail.com>
2022-06-14 00:18:29 -0300

    Entities Configurations and dbContext created

---
+++ CleanArch.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(y => y.Name).HasMaxLength(100).IsRequired();
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios"));
+            
+        }
+    }
+}
---
+++ CleanArch.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(y => y.Name).HasMaxLength(100).IsRequired();
+            builder.Property(y => y.Description).HasMaxLength(200).IsRequired();
+            builder.Property(y => y.Price).HasPrecision(10, 2);
+            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
